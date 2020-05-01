c5f56c0eb110dbeb0d2bbba6ebab013b86c30afb
Jamie Shiell <jamie@infernus.org>
2020-05-01 09:00:10 +0100

    Added a simple line drawing function.

---
+++ src/main/kotlin/org/infernus/tinyrenderer/TinyRenderer.kt
@@ -0,0 +1,54 @@
+package org.infernus.tinyrenderer
+
+import java.awt.FlowLayout
+import java.awt.image.BufferedImage
+import java.awt.image.DataBufferInt
+import javax.swing.ImageIcon
+import javax.swing.JFrame
+import javax.swing.JLabel
+
+
+class TinyRenderer(private val width: Int,
+                   private val height: Int) {
+
+    private val pixels = IntArray(width * height)
+
+    fun drawLine(x0: Int, y0: Int, x1: Int, y1: Int, colour: Int) {
+        var t = 0.0f
+        while (t < 1.0) {
+            val x = (x0 + (x1 - x0) * t).toInt()
+            val y = (y0 + (y1 - y0) * t).toInt()
+            pixels[x + y * width] = colour
+            t += .01f
+        }
+    }
+
+    fun asBufferedImage(): BufferedImage {
+        val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        val imagePixels = (image.raster.dataBuffer as DataBufferInt).data
+        IntRange(0, pixels.size - 1).forEach { index ->
+            imagePixels[index] = pixels[index]
+        }
+        return image
+    }
+
+    companion object {
+        const val COLOUR_WHITE = 0xFFFFFF
+    }
+}
+
+fun main() {
+    val renderer = TinyRenderer(100, 100)
+    renderer.drawLine(20, 20, 80, 70, TinyRenderer.COLOUR_WHITE)
+
+    showImageInFrame(renderer.asBufferedImage())
+}
+
+private fun showImageInFrame(image: BufferedImage) {
+    val frame = JFrame()
+    frame.contentPane.layout = FlowLayout()
+    frame.contentPane.add(JLabel(ImageIcon(image)))
+    frame.pack()
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE;
+    frame.isVisible = true
+}
